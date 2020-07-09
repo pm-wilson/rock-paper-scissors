a6f0f905fc21588469e037f66aa833dbc0c1cb3e
Patrick Wilson <pmwilson333@gmail.com>
2020-07-08 19:42:32 -0700

    design complete

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // import functions and grab DOM elements
+const userMove = document.querySelector("input[name = user-select]:checked"),
+  playButton = document.querySelector("#trial-button"),
+  totalDisplay = document.querySelector("#reset-button"),
+  totalWin = document.querySelector("#reset-button"),
+  totalLoss = document.querySelector("#reset-button"),
+  totalTied = document.querySelector("#reset-button"),
+  totalReset = document.querySelector("#reset-button");
 
 // initialize state
 
 // set event listeners to update state and DOM
+playButton.addEventListener("click", () => {
+  console.log(userMove.value);
+});
